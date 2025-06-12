46183851a0d14b4a34f892483b43749d1af96dde
ClassyCoder <classycoder1@gmail.com>
2025-06-12 14:45:16 +0530

    fix: guis, i messed them up in last commit

--- src/main/java/io/github/arctyll/veyzen/gui/modmenu/ModMenu.java
+++ src/main/java/io/github/arctyll/veyzen/gui/modmenu/ModMenu.java
@@ -1,80 +1,119 @@
+/*
+
+ Copyright (c) 2022 DupliCAT
+
+ GNU Lesser General Public License v3.0
+ */
+
+
 package io.github.arctyll.veyzen.gui.modmenu;
 
 import io.github.arctyll.veyzen.Veyzen;
+import io.github.arctyll.veyzen.gui.Style;
 import io.github.arctyll.veyzen.gui.modmenu.impl.Panel;
+import io.github.arctyll.veyzen.helpers.ResolutionHelper;
 import io.github.arctyll.veyzen.helpers.TimeHelper;
 import io.github.arctyll.veyzen.helpers.render.Helper2D;
+import io.github.arctyll.veyzen.helpers.MathHelper;
 import io.github.arctyll.veyzen.helpers.animation.Animate;
 import io.github.arctyll.veyzen.helpers.animation.Easing;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.util.ResourceLocation;
 
-import java.awt.*;
 import java.io.IOException;
+import java.awt.*;
 
 public class ModMenu extends GuiScreen {
 
-    private final Panel panel = new Panel();
-    private final Animate animateClock = new Animate();
-    private final Animate animateSnapping = new Animate();
-    private int scale = -1;
+	private final Panel panel = new Panel();  
 
-    public ModMenu() {
-        animateClock.setEase(Easing.CUBIC_OUT).setMin(0).setMax(50).setSpeed(100).setReversed(false);
-        animateSnapping.setEase(Easing.CUBIC_IN).setMin(0).setMax(50).setSpeed(100).setReversed(false);
-    }
+	private final Animate animateClock = new Animate();  
+	private final Animate animateSnapping = new Animate();  
 
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        Helper2D.drawRectangle(0, 0, width, height, 0x70000000);
-        boolean rounded = Veyzen.INSTANCE.optionManager.getOptionByName("Rounded Corners").isCheckToggled();
+	public ModMenu() {  
+		animateClock.setEase(Easing.CUBIC_OUT).setMin(0).setMax(50).setSpeed(100).setReversed(false);  
+		animateSnapping.setEase(Easing.CUBIC_IN).setMin(0).setMax(50).setSpeed(100).setReversed(false);  
+	}  
 
-        panel.renderPanel(mouseX, mouseY);
+	/**  
+	 * Draws the panel of the modmenu used to toggle mods and change settings  
+	 *  
+	 * @param mouseX The current X position of the mouse  
+	 * @param mouseY The current Y position of the mouse  
+	 * @param partialTicks The partial ticks used for rendering  
+	 */  
 
-        animateClock.update();
+	@Override  
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {  
+		Helper2D.drawRectangle(0, 0, width, height, 0x70000000);  
+		boolean roundedCorners = Veyzen.INSTANCE.optionManager.getOptionByName("Rounded Corners").isCheckToggled();  
+		int color = Veyzen.INSTANCE.optionManager.getOptionByName("Color").getColor().getRGB();  
 
-        Helper2D.drawRoundedRectangle(width - 130, animateClock.getValueI() - 60, 140, 60, 10,
-									  new Color(25, 103, 255).getRGB(), rounded ? 0 : -1);
-        Helper2D.drawPicture(width - 50, 5 - 50 + animateClock.getValueI(), 40, 40,
-							 new Color(255, 255, 255).getRGB(), "icon/clock.png");
-        Veyzen.INSTANCE.fontHelper.size40.drawString(TimeHelper.getFormattedTimeMinute(),
-													 width - 120, 10 - 50 + animateClock.getValueI(), Color.WHITE.getRGB());
-        Veyzen.INSTANCE.fontHelper.size20.drawString(TimeHelper.getFormattedDate(),
-													 width - 120, 30 - 50 + animateClock.getValueI(), Color.WHITE.getRGB());
+		panel.renderPanel(mouseX, mouseY);  
 
-        animateSnapping.update();
-    }
+		/*  
+		 Draws the time at the top right  
+		 */  
 
-    @Override
-    public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        panel.mouseClicked(mouseX, mouseY, mouseButton);
-        super.mouseClicked(mouseX, mouseY, mouseButton);
-    }
+		animateClock.update();  
 
-    @Override
-    public void mouseReleased(int mouseX, int mouseY, int state) {
-        panel.setDragging(false);
-        panel.mouseReleased(mouseX, mouseY, state);
-        super.mouseReleased(mouseX, mouseY, state);
-    }
+		Helper2D.drawRoundedRectangle(width - 130, animateClock.getValueI() - 60, 140, 60, 10, new Color(25, 103, 255, 255).getRGB(), roundedCorners ? 0 : -1);  
+		Helper2D.drawPicture(width - 50, 5 - 50 + animateClock.getValueI(), 40, 40, new Color(255, 255, 255, 255).getRGB(), "icon/clock.png");  
 
-    @Override
-    public void keyTyped(char typedChar, int keyCode) throws IOException {
-        panel.keyTyped(typedChar, keyCode);
-        super.keyTyped(typedChar, keyCode);
-    }
+		Veyzen.INSTANCE.fontHelper.size40.drawString(TimeHelper.getFormattedTimeMinute(), width - 120, 10 - 50 + animateClock.getValueI(), Color.WHITE.getRGB());  
+		Veyzen.INSTANCE.fontHelper.size20.drawString(TimeHelper.getFormattedDate(), width - 120, 30 - 50 + animateClock.getValueI(), Color.WHITE.getRGB());  
 
-    @Override
-    public void initGui() {
-        panel.initGui();
-        scale = mc.gameSettings.guiScale;
-        mc.gameSettings.guiScale = 1;
-        super.initGui();
-    }
+		animateSnapping.update();  
+	}  
 
-    @Override
-    public void onGuiClosed() {
-        if (scale != -1) mc.gameSettings.guiScale = scale;
-        super.onGuiClosed();
-    }
+	/**  
+	 * Sets different values of the panel when any mouse button is clicked  
+	 * Changes the darkMode boolean if the button in the bottom left is pressed  
+	 *  
+	 * @param mouseX The current X position of the mouse  
+	 * @param mouseY The current Y position of the mouse  
+	 * @param mouseButton The current mouse button which is pressed  
+	 */  
+
+	@Override  
+	public void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {  
+		panel.mouseClicked(mouseX, mouseY, mouseButton);  
+		super.mouseClicked(mouseX, mouseY, mouseButton);  
+	}  
+
+	@Override  
+	public void mouseReleased(int mouseX, int mouseY, int state) {  
+		panel.setDragging(false);  
+		panel.mouseReleased(mouseX, mouseY, state);  
+		super.mouseReleased(mouseX, mouseY, state);  
+	}  
+
+	@Override  
+	public void keyTyped(char typedChar, int keyCode) throws IOException {  
+		panel.keyTyped(typedChar, keyCode);  
+		super.keyTyped(typedChar, keyCode);  
+	}  
+
+	/**  
+	 * Loads a shader to blur the screen when the gui is opened  
+	 */  
+
+	@Override  
+	public void initGui() {  
+		panel.initGui();  
+		mc.entityRenderer.loadShader(new ResourceLocation("shaders/post/blur.json"));  
+		super.initGui();  
+	}  
+
+	/**  
+	 * Deleted all shaderGroups in order to remove the screen blur when the gui is closed  
+	 */  
+
+	@Override  
+	public void onGuiClosed() {  
+		if (mc.entityRenderer.getShaderGroup() != null) {  
+			mc.entityRenderer.getShaderGroup().deleteShaderGroup();  
+		}  
+		super.onGuiClosed();  
+	}
 }
